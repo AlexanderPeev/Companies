7178ba42162fd529a989bd35ab771eaec0a5b398
Alexander Peev <peev.alexander@gmail.com>
2019-10-18 13:56:49 +0200

    Trying to get Heroku container to wait for DB to be ready.

--- server/companies/src/main.ts
+++ server/companies/src/main.ts
@@ -4,14 +4,33 @@ const execFile = require("child_process").execFile;
 
 export class Main {
     public main(): void {
-        const clientDir = process.argv[2];
+        const clientDir = process.argv[2] + '';
         const port = parseInt(process.argv[3], 10);
         const dbStart = process.argv[4] || null;
+        let started = false;
         console.log('Setting up REST interfaces with client dir: ' + clientDir);
 
         if(dbStart) {
-            execFile(dbStart);
+            console.log('Starting DB: ' + dbStart);
+            const dbProcess = execFile(dbStart);
+            dbProcess.stdout.on('data', (data: any) => {
+                console.log('DB stdout: ' + data);
+                if(data.indexOf('Server ready')) {
+                    if(!started) {
+                        this.startRestService(clientDir, port);
+                        started = true;
+                    }
+                }
+            });
+            dbProcess.stderr.on('data', (data: any) => {
+                console.log('DB stderr: ' + data);
+            });
+        } else {
+            this.startRestService(clientDir, port);
         }
+    }
+
+    private startRestService(clientDir: string, port: number): void {
         new StorageEngine().prepare().then(storage => new Rest().setup(clientDir, storage, port)).catch(e => {
             console.error(e);
         });
